5fa68ec3ebf2bdf5d205ac5cc41afc3fa3829df2
Ruth-Mwangi <rwmwangi96@gmail.com>
2020-02-19 11:47:52 +0300

    Updates made to index,styles and js files

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    $(".js").click(function(){
+        $("#hJs").fadeToggle();
+        $("#sJs").fadeToggle();
+
+    });
+    $(".op").click(function(){
+        $("#hOp").fadeToggle();
+        $("#sOp").fadeToggle();
+
+    });
+    $(".va").click(function(){
+        $("#hVa").fadeToggle();
+        $("#sVa").fadeToggle();
+
+    });
+    $(".fu").click(function(){
+        $("#hFu").fadeToggle();
+        $("#sFu").fadeToggle();
+
+    });
+    $(".st").click(function(){
+        $("#hSt").fadeToggle();
+        $("#sSt").fadeToggle();
+
+    });
+
+});
